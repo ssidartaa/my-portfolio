ce4a8519761f8b0c50e821e6337fc4a6da349ccd
ssidartaa <odssy351@gmail.com>
2023-01-31 12:46:56 -0500

    feat: CNAB deply link

--- src/utils/deployData.ts
+++ src/utils/deployData.ts
@@ -1,8 +1,32 @@
 export const deployData = [
-    {name: "my-portfolio", deployLink: "https://my-portfolio-rho-indol.vercel.app/"},
-    {name: "transactions-calculator", deployLink: "https://transactions-calculator.vercel.app/"},
-    {name: "kenzie-hub-tsx", deployLink: "https://react-entrega-s2-formulario-de-cadastro-ssidartaa.vercel.app/"},
-    {name: "MARATONA-PICPRO", deployLink: "https://ssidartaa.github.io/MARATONA-PICPRO/"},
-    {name: "MARATONA-VIII-SORTEIO-DE-NOMES", deployLink: "https://ssidartaa.github.io/MARATONA-VIII-SORTEIO-DE-NOMES/"},
-    {name:"MARATONA-KENZIEFLIX", deployLink: "https://ssidartaa.github.io/MARATONA-KENZIEFLIX/"},
-]
+  {
+    name: "CNAB-parser-api",
+    deployLink: "https://cnab-parser-api.onrender.com/docs/swagger-ui/",
+  },
+  { name: "CNAB-parser", deployLink: "https://cnab-parser.vercel.app/" },
+  {
+    name: "my-portfolio",
+    deployLink: "https://my-portfolio-rho-indol.vercel.app/",
+  },
+  {
+    name: "transactions-calculator",
+    deployLink: "https://transactions-calculator.vercel.app/",
+  },
+  {
+    name: "kenzie-hub-tsx",
+    deployLink:
+      "https://react-entrega-s2-formulario-de-cadastro-ssidartaa.vercel.app/",
+  },
+  {
+    name: "MARATONA-PICPRO",
+    deployLink: "https://ssidartaa.github.io/MARATONA-PICPRO/",
+  },
+  {
+    name: "MARATONA-VIII-SORTEIO-DE-NOMES",
+    deployLink: "https://ssidartaa.github.io/MARATONA-VIII-SORTEIO-DE-NOMES/",
+  },
+  {
+    name: "MARATONA-KENZIEFLIX",
+    deployLink: "https://ssidartaa.github.io/MARATONA-KENZIEFLIX/",
+  },
+];
